ecc452e1f1e07bd7b6f688eac4fe6e70f5fd4203
MichaelCampbell2020 <cam3000@gmail.com>
2022-03-31 16:51:58 -0700

    test suite passes

--- src/models/index.js
+++ src/models/index.js
@@ -1,8 +1,8 @@
 'use strict';
 
 const { Sequelize, DataTypes } = require('sequelize');
-const clothesModel = require('./clothes/model.js');
-const foodModel = require('./food/model.js');
+const clothesModel = require('./categories/model.js');
+const foodModel = require('./items/model.js');
 const Collection = require('./data-collection.js');
 
 const DATABASE_URL = process.env.DATABASE_URL || 'sqlite:memory:';
