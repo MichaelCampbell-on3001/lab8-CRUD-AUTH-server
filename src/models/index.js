e64fa814c5471a30bcca3e6d8e370ce60b640aa1
MichaelCampbell2020 <cam3000@gmail.com>
2022-04-03 16:23:29 -0700

    authserver db config

--- src/models/index.js
+++ src/models/index.js
@@ -5,7 +5,9 @@ const categoriesModel = require('./categories/model.js');
 const itemsModel = require('./items/model.js');
 const Collection = require('./data-collection.js');
 
-const DATABASE_URL = process.env.DATABASE_URL || 'sqlite:memory:';
+const DATABASE_URL = process.env.DATABASE_URL || 'sqlite::memory:';
+
+
 
 const sequelize = new Sequelize(DATABASE_URL);
 const items = itemsModel(sequelize, DataTypes);
