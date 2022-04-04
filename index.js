2e551ae746a6da4b6897361f913b7f3909a25d63
MichaelCampbell2020 <cam3000@gmail.com>
2022-04-03 17:12:52 -0700

    commented out index js extra stuff

--- index.js
+++ index.js
@@ -1,46 +1,46 @@
 'use strict';
 
-require('dotenv').config();
+
 const app = require('./src/server.js');
 const { db } = require('./src/auth/models');
-const base64 = require('base-64');
-const bcrypt = require('bcrypt');
+// const base64 = require('base-64');
+// const bcrypt = require('bcrypt');
 
-let username = 'Username';
-let password = 'Password';
+// let username = 'Username';
+// let password = 'Password';
 
-let authHeaderString = `${username}:${password}`;
+// let authHeaderString = `${username}:${password}`;
 
-let encodedHeader = base64.encode(authHeaderString);
+// let encodedHeader = base64.encode(authHeaderString);
 
-console.log(encodedHeader);
-let decodedHeader = base64.decode(encodedHeader);
-console.log(decodedHeader);
+// console.log(encodedHeader);
+// let decodedHeader = base64.decode(encodedHeader);
+// console.log(decodedHeader);
 
-let passwordFromHeader = decodedHeader.split(':')[1];
+// let passwordFromHeader = decodedHeader.split(':')[1];
 
-async function encrypt(password) {
+// async function encrypt(password) {
 
-  let complexity = 5;
+//   let complexity = 5;
 
-  let hashedPassword = await bcrypt.hash(password, complexity);
+//   let hashedPassword = await bcrypt.hash(password, complexity);
 
-  // this is safe to store.
-  return hashedPassword;
-}
+//   // this is safe to store.
+//   return hashedPassword;
+// }
 
-async function compare(password, hash) {
+// async function compare(password, hash) {
 
-  let isValid = await bcrypt.compare(password, hash);
+//   let isValid = await bcrypt.compare(password, hash);
 
-  console.log(isValid);
-}
+//   console.log(isValid);
+// }
 
-encrypt(passwordFromHeader)
-  .then(hash => {
-    console.log(hash);
-    compare('wrong', hash);
-  });
+// encrypt(passwordFromHeader)
+//   .then(hash => {
+//     console.log(hash);
+//     compare('wrong', hash);
+//   });
 
   db.sync().then(() => {
     app.start(process.env.PORT || 3001);
